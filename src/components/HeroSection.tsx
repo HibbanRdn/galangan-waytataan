8dc25da57b016cb46e667e2565744029ee5185dd
HibbanRdn <muhamadhibbanramadhan@gmail.com>
2026-01-20 13:06:08 +0700

    refactor: remove scroll indicator from HeroSection component

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -99,9 +99,6 @@ export function HeroSection() {
 
           {/* Scroll Indicator */}
           <div className="absolute bottom-12 left-1/2 -translate-x-1/2 flex flex-col items-center gap-2 animate-bounce">
-            <div className="w-6 h-10 border-2 border-white/30 rounded-full flex items-start justify-center p-2">
-              <div className="w-1.5 h-2 bg-white/50 rounded-full"></div>
-            </div>
             <span className="text-white/50 text-xs tracking-widest">SCROLL</span>
           </div>
         </div>
